3be00acc8dded70da52cd023f6b6a4c3e30238b4
Jacob Morrison <Jacob.Morrison@vai.org>
2025-11-21 12:04:22 -0500

    finish descriptive stats plots

--- code/C_descriptive_stats.py
+++ code/C_descriptive_stats.py
@@ -12,7 +12,9 @@ def by_sample(df):
     fig = plt.figure(figsize=(10, 5))
     plt.tight_layout()
 
-    plt.boxplot(df)
+    bp = plt.boxplot(df, sym='k.')
+
+    plt.setp(bp['fliers'], markersize=1)
 
     plt.title('Methylation Levels By Sample')
     plt.xlabel('')
@@ -20,29 +22,50 @@ def by_sample(df):
 
     plt.ylim(0, 1.05)
 
-    plt.xticks(ticks=range(1,len(df.columns)+1), labels=df.columns, rotation=90)
+    fontsize = 10 if len(df.columns) <= 10 else 6
+
+    plt.xticks(ticks=range(1,len(df.columns)+1), labels=df.columns, rotation=90, fontsize=fontsize)
 
     plt.savefig('stats_by_sample.pdf', bbox_inches='tight')
     plt.close('all')
 
     return None
 
-def by_cpg(df):
+def by_cpg_cell_type(df):
     """Create lineplot of statistics by CpG"""
-    mean = df.mean(axis=1)
+    e_samples = [x for x in df.columns if x.endswith('E_raw')]
+    s_samples = [x for x in df.columns if x.endswith('S_raw')]
+    e_mean = df[e_samples].mean(axis=1)
+    s_mean = df[s_samples].mean(axis=1)
 
     fig, ax = plt.subplots(figsize=(5, 5))
     plt.tight_layout()
 
-    ax.violinplot(mean, widths=0.1, showmeans=True, showmedians=True)
+    parts = ax.violinplot(
+        [e_mean, s_mean],
+        positions=[1.0, 1.3],
+        widths=0.2,
+        showmeans=True,
+        showmedians=True
+    )
+
+    colors = ['#377eb8', '#e41a1c']
+    for i, part in enumerate(parts['bodies']):
+        part.set_facecolor(colors[i])
+        part.set_edgecolor('black')
+        part.set_alpha(1)
+
+    parts['cmeans'].set_color('black')
+    for key in ['cmins', 'cmaxes', 'cbars', 'cmedians']:
+        parts[key].set_color('#bdbdbd')
 
     plt.title('Mean Methylation Levels Across All CpGs')
-    plt.xlabel('CpG')
+    plt.xlabel('')
     plt.ylabel('Methylation Level')
 
-    plt.xlim(0.8,1.2)
+    plt.xlim(0.8,1.5)
     plt.ylim(0, 1.05)
-    plt.xticks([])
+    plt.xticks(ticks=[1,1.3], labels=['Epithelial', 'Stromal'])
 
     plt.savefig('stats_by_cpg.pdf', bbox_inches='tight')
     plt.close('all')
@@ -58,7 +81,7 @@ def main(df):
     by_sample(df_raw)
 
     logger.info('Generating by-CpG descriptive statistics plot')
-    by_cpg(df_raw)
+    by_cpg_cell_type(df_raw)
 
     return None
 
